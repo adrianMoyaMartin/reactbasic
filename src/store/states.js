9466d23fb30bb1001735d8ce9eeac623c5b3133d
adrianMoyaMartin <125206876+adrianMoyaMartin@users.noreply.github.com>
2023-02-14 19:39:01 +0100

    working zustand persist

--- src/store/states.js
+++ src/store/states.js
@@ -1,9 +1,15 @@
 import { create } from "zustand";
 import { apiData } from "./storeApiData";
 import { countData } from "./storeCounter";
+import { persist } from "zustand/middleware";
 
-const useStorage = create((...a) => ({
-  ...apiData(...a),
-  ...countData(...a),
-}));
+const useStorage = create(
+  persist(
+    (...a) => ({
+      ...apiData(...a),
+      ...countData(...a),
+    }),
+    { name: "caca2", partialize: (state) => ({ count: state.count }) }
+  )
+);
 export default useStorage;
